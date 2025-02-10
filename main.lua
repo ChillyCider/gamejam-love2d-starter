da4f96395f2bcd607862dee0630b45bacb66a4ca
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-10 12:37:06 -0500

    Use a loader Proxy instead of scanning directory at the start

--- main.lua
+++ main.lua
@@ -2,77 +2,48 @@ local GS = require "hump.gamestate"
 local Timer = require "hump.timer"
 local AsepriteSheet = require "support.aseprite_sheet"
 
-_G.images = {} ---@type table<string, love.Image>
-_G.sheets = {} ---@type table<string, AsepriteSheet>
-_G.fonts = {} ---@type table<string, love.Font>
-_G.sounds = {} ---@type table<string, love.Source>
-_G.music = {} ---@type table<string, love.Source>
-_G.states = {} ---@type table<string, table>
+-- based on vrld's proxy
+local function Proxy(loadFunc)
+    return setmetatable({}, {__index=function(t, key)
+        local v = loadFunc(key)
+        rawset(t, key, v)
+        return v
+    end})
+end
+
+---@type table<string, love.Image>
+_G.images = Proxy(function(k)
+    if love.filesystem.getInfo("assets/images/" .. k .. ".png") then
+        return love.graphics.newImage("assets/images/" .. k .. ".png")
+    end
+
+    return love.graphics.newImage("assets/images/" .. k .. ".jpg")
+end)
+---@type table<string, AsepriteSheet>
+_G.sheets = Proxy(function(k)
+    return AsepriteSheet.new(_G.images[k], "assets/images/" .. k .. ".json")
+end)
+---@type table<string, love.Font>
+_G.fonts = Proxy(function(k)
+    if love.filesystem.getInfo("assets/fonts/" .. k .. ".fnt") then
+        return love.graphics.newFont("assets/fonts/" .. k .. ".fnt")
+    end
+    return love.graphics.newFont("assets/fonts/" .. k .. ".ttf")
+end)
+---@type table<string, love.Source>
+_G.sounds = Proxy(function(k)
+    return love.audio.newSource("assets/sounds/" .. k .. ".wav", "static")
+end)
+---@type table<string, love.Source>
+_G.music = Proxy(function(k)
+    return love.audio.newSource("assets/music/" .. k .. ".ogg", "stream")
+end)
+---@type table<string, table>
+_G.states = Proxy(function(k)
+    return assert(love.filesystem.load("states/" .. k .. ".lua"))()
+end)
 
 function love.load()
-    -- IMAGES
-    do
-        local fileNames = love.filesystem.getDirectoryItems("assets/images/")
-        for _, name in ipairs(fileNames) do
-            if name:match("%.png$") or name:match("%.jpg$") then
-                local img = love.graphics.newImage("assets/images/" .. name)
-                local withoutExt = name:gsub("(%.%w+)$", "")
-                _G.images[withoutExt] = img
-
-                -- Try to load asesprite animation too if possible
-                local jsonPath = "assets/images/" .. withoutExt .. ".json"
-                if love.filesystem.getInfo(jsonPath, "file") then
-                    _G.sheets[withoutExt] = AsepriteSheet.new(img, jsonPath)
-                end
-            end
-        end
-    end
-
-    -- FONTS
-    do
-        local fileNames = love.filesystem.getDirectoryItems("assets/fonts/")
-        for _, name in ipairs(fileNames) do
-            if name:match("%.fnt$") or name:match("%.ttf$") then
-                local fnt = love.graphics.newFont("assets/fonts/" .. name)
-                _G.fonts[name:gsub("(%.%w+)$", "")] = fnt
-            end
-        end
-    end
-
-    -- SOUNDS
-    do
-        local fileNames = love.filesystem.getDirectoryItems("assets/sounds/")
-        for _, name in ipairs(fileNames) do
-            if name:match("%.wav$") then
-                local snd = love.audio.newSource("assets/sounds/" .. name, "static")
-                _G.sounds[name:gsub("(%.%w+)$", "")] = snd
-            end
-        end
-    end
-
-    -- MUSIC
-    do
-        local fileNames = love.filesystem.getDirectoryItems("assets/music/")
-        for _, name in ipairs(fileNames) do
-            if name:match("%.ogg$") then
-                local mus = love.audio.newSource("assets/music/" .. name, "stream")
-                mus:setLooping(true)
-                _G.music[name:gsub("(%.%w+)$", "")] = mus
-            end
-        end
-    end
-
-    -- GAME STATES
-    do
-        local fileNames = love.filesystem.getDirectoryItems("states/")
-        for _, name in ipairs(fileNames) do
-            local moduleName = name:match("([^%.]+)%.lua$")
-            if moduleName then
-                _G.states[moduleName] = require("states." .. moduleName)
-            end
-        end
-    end
-
     GS.registerEvents()
 
     -- Switch to the first state
