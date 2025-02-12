a98e527fd96ca32ec71b4a48232527f8c196ea7d
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-12 03:33:21 -0500

    Add speedFactor to AsepriteAnimPlayer

--- support/aseprite_anim_player.lua
+++ support/aseprite_anim_player.lua
@@ -3,6 +3,7 @@
 
 ---@class AsepriteAnimPlayer
 ---@field paused boolean Whether to pause the animation.
+---@field speedFactor number Speed at which to play the animation. 1 means normal, 0 means frozen.
 local AsepriteAnimPlayer = {}
 local MT = {__index=AsepriteAnimPlayer}
 
@@ -16,6 +17,7 @@ function AsepriteAnimPlayer:play(asepriteSheet, tagName, loops, forceRestart)
     self.paused = false
     self.loops = loops or 0
     self.loopsDone = 0
+    self.speedFactor = 1
 
     if self.asepriteSheet ~= asepriteSheet or self.tagName ~= tagName or forceRestart then
         self.asepriteSheet = asepriteSheet
@@ -52,25 +54,26 @@ end
 ---@param dt number Number of seconds.
 function AsepriteAnimPlayer:update(dt)
     if not paused and (self.loops <= 0 or self.loopsDone < self.loops) then
-        self.frameTimer = self.frameTimer - dt
-        if self.frameTimer <= 0.0 then
-            local from, to, direction
+        local from, to, direction
 
-            local tag = nil
-            if self.tagName then
-                tag = self.asepriteSheet:tag(self.tagName)
-            end
+        local tag = nil
+        if self.tagName then
+            tag = self.asepriteSheet:tag(self.tagName)
+        end
 
-            if tag then
-                from = tag.from + 1
-                to = tag.to + 1
-                direction = tag.direction
-            else
-                from = 1
-                to = #self.asepriteSheet.data.frames
-                direction = "forward"
-            end
-            
+        if tag then
+            from = tag.from + 1
+            to = tag.to + 1
+            direction = tag.direction
+        else
+            from = 1
+            to = #self.asepriteSheet.data.frames
+            direction = "forward"
+        end
+
+        self.frameTimer = self.frameTimer - dt*self.speedFactor
+
+        while self.frameTimer <= 0.0 and not paused and (self.loops <= 0 or self.loopsDone < self.loops) do
             local oldFrame = self.frameIndex
 
             if direction == "forward" then
@@ -113,7 +116,7 @@ function AsepriteAnimPlayer:update(dt)
                 self.frameIndex = oldFrame
             end
 
-            self.frameTimer = self.asepriteSheet.data.frames[self.frameIndex].duration / 1000
+            self.frameTimer = self.frameTimer + self.asepriteSheet.data.frames[self.frameIndex].duration / 1000
         end
     end
 end
@@ -148,6 +151,7 @@ return function(asepriteSheet, tagName, loops)
         paused=false,
         loops=loops or 0,
         loopsDone=0,
+        speedFactor=1
     }, MT)
 
     obj:play(asepriteSheet, tagName, loops)
