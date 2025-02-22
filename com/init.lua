dde23ed85352c9edf2d838b216e5f402daf8e549
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-21 22:18:43 -0500

    Remove ECS and ui layout stuff

--- com/init.lua
+++
@@ -1,9 +0,0 @@
--- (c) 2025 Charlie Murphy
--- This code is licensed under MIT license (see LICENSE.txt for details)
-
-return setmetatable({
-}, {__index=function(t, name)
-    local m = require("com." .. name)
-    rawset(t, name, m)
-    return m
-end})
--- ui/init.lua
+++
@@ -1,6 +0,0 @@
--- (c) 2025 Charlie Murphy
--- This code is licensed under MIT license (see LICENSE.txt for details)
-
-return {
-    r=require "ui.r"
-}
