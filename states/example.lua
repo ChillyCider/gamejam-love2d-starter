da22869ef82313356931f9dfe496e16493fcddc7
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-21 22:22:10 -0500

    Remove all usage of ECS

--- states/example.lua
+++ states/example.lua
@@ -1,34 +1,18 @@
 -- (c) 2025 Charlie Murphy
 -- This code is licensed under MIT license (see LICENSE.txt for details)
 
-local ecs = require "ecs"
-local com = require "com"
-local util = require "util"
-local world = ecs.World():registerSystemsFromDir("systems")
-
 local example = {}
 
 function example:enter()
-    world:add {
-        com.Pos(100, 100),
-        com.Sprite(_G.images.mantis),
-    }
-    world:add {
-        com.Pos(120, 130),
-        com.Sprite(_G.images.mantis),
-    }
 end
 
 function example:leave()
-    world:wipe()
 end
 
 function example:update(dt)
-    world:update(dt)
 end
 
 function example:draw()
-    world:draw()
 end
 
 return example
