010c29a3bed5215d8365a750b4a88bbc307876f9
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-22 04:39:23 -0500

    justPressed and justReleased

--- main.lua
+++ main.lua
@@ -7,6 +7,10 @@ support = require "support"
 util = require "util"
 Sprite = require "Sprite"
 Group = require "Group"
+---@type table<love.Scancode, boolean>
+justPressed = {}
+---@type table<love.Scancode, boolean>
+justReleased = {}
 
 time = 0.0
 TIME_ROLLOVER = 3600.0
@@ -36,8 +40,19 @@ function love.update(dt)
     time = util.wrap(time + dt, 0.0, TIME_ROLLOVER)
     util.Timers:update(dt)
     mantises:update(dt)
+
+    for k,v in pairs(justPressed) do if v then justPressed[k] = false end end
+    for k,v in pairs(justReleased) do if v then justReleased[k] = false end end
 end
 
 function love.draw()
     mantises:draw()
+end
+
+function love.keypressed(_, scancode, _)
+    justPressed[scancode] = true
+end
+
+function love.keyreleased(_, scancode, _)
+    justReleased[scancode] = true
 end
