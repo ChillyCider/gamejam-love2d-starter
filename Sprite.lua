582310ee7c14cf342d9f706b37555aead4937129
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-22 04:21:57 -0500

    More Sprite changes

--- Sprite.lua
+++ Sprite.lua
@@ -4,6 +4,15 @@
 ---@class Sprite
 ---@field x number
 ---@field y number
+---@field w number
+---@field h number
+---@field visible boolean
+---@field active boolean
+---@field rotation number
+---@field scaleX number
+---@field scaleY number
+---@field originX number
+---@field originY number
 ---@field image love.Image
 ---@field animPlayer support.AsepriteAnimPlayer?
 ---@field drawMode "img"|"anim"
@@ -15,9 +24,9 @@ end
 
 function Sprite:draw()
     if self.drawMode == "img" then
-        love.graphics.draw(self.image, self.x, self.y)
+        love.graphics.draw(self.image, self.x, self.y, self.rotation, self.scaleX, self.scaleY, self.originX, self.originY)
     elseif self.drawMode == "anim" then
-        self.animPlayer:draw(self.x, self.y)
+        self.animPlayer:draw(self.x, self.y, self.rotation, self.scaleX, self.scaleY, self.originX, self.originY)
     end
 end
 
@@ -36,25 +45,27 @@ function Sprite:play(sheet, tag, loops, forceRestart)
 end
 
 function Sprite:setHitboxFromImage()
-    if self.drawMode == "img" then
+    if self.drawMode == "img" and self.image then
         self.w = self.image:getWidth()
         self.h = self.image:getHeight()
-    elseif self.drawMode == "anim" then
+    elseif self.drawMode == "anim" and self.animPlayer then
         self.w = self.animPlayer.asepriteSheet.data.frames[self.animPlayer.frameIndex].frame.w
         self.h = self.animPlayer.asepriteSheet.data.frames[self.animPlayer.frameIndex].frame.h
     end
     return self
 end
 
----@param x number
----@param y number
----@param imgOrAnimSheet love.Image|support.AsepriteSheet
+---@param x? number
+---@param y? number
+---@param imgOrAnimSheet? love.Image|support.AsepriteSheet
+---@param tag string?
+---@param loops number?
 function Sprite:new(x, y, imgOrAnimSheet, tag, loops)
     self.__index = self
     local img = nil
     local animPlayer = nil
     local drawMode
-    if imgOrAnimSheet.tag then
+    if imgOrAnimSheet and imgOrAnimSheet.tag then
         animPlayer = support.AsepriteAnimPlayer(
             imgOrAnimSheet --[[@as support.AsepriteSheet]],
             tag,
@@ -66,10 +77,17 @@ function Sprite:new(x, y, imgOrAnimSheet, tag, loops)
         drawMode = "img"
     end
     return setmetatable({
-        x=x,
-        y=y,
+        x=x or 0,
+        y=y or 0,
         w=0,
         h=0,
+        visible=true,
+        active=true,
+        rotation=0,
+        scaleX=1,
+        scaleY=1,
+        originX=0,
+        originY=0,
         image=img,
         animPlayer=animPlayer,
         drawMode=drawMode
--- main.lua
+++ main.lua
@@ -5,14 +5,54 @@
 R = require "R"
 support = require "support"
 util = require "util"
+Sprite = require "Sprite"
+
+time = 0.0
+TIME_ROLLOVER = 3600.0
+
+---@param sprites Sprite[]
+---@param dt number
+local function updateAll(sprites, dt)
+    for _, spr in ipairs(sprites) do
+        if spr.active then spr:update(dt) end
+    end
+end
+
+---@param sprites Sprite[]
+local function drawAll(sprites)
+    for _, spr in ipairs(sprites) do
+        if spr.visible then spr:draw() end
+    end
+end
+
+Mantis = Sprite:new()
+function Mantis:update(dt)
+    Sprite.update(self, dt)
+    self.rotation = math.cos(time)
+end
 
 function love.load()
+    sprites = {}
+    for _=1,10 do
+        table.insert(
+            sprites,
+            Mantis:new(
+                math.random(0, love.graphics.getWidth()),
+                math.random(0, love.graphics.getHeight()),
+                R.sheets.mantis
+            )
+        )
+    end
 end
 
 ---@param dt number
 function love.update(dt)
+    time = time + dt
+    if time > TIME_ROLLOVER then time = (time - TIME_ROLLOVER) end
+    updateAll(sprites, dt)
     util.Timers:update(dt)
 end
 
 function love.draw()
+    drawAll(sprites)
 end
