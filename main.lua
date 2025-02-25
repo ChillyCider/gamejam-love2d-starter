b3630de197f7a4666223f0e1b1e5ba6ef9bf660a
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-25 01:55:14 -0500

    Document main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,8 @@
+-- (c) 2025 Charlie Murphy
+-- This code is licensed under MIT license (see LICENSE.txt for details)
+
+-- src/ is where Lua files will now be searched for
 love.filesystem.setRequirePath("src/?.lua;src/?/init.lua")
+
+-- Jump immediately to entrypoint code in src/
 require "entrypoint"
