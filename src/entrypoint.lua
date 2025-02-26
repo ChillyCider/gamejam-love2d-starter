d7af50c641d19989b0d98521ada448327f9c96e8
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-25 20:17:19 -0500

    Forgot to commit this file

--- src/entrypoint.lua
+++ src/entrypoint.lua
@@ -1,11 +1,10 @@
 -- (c) 2025 Charlie Murphy
 -- This code is licensed under MIT license (see LICENSE.txt for details)
 
-globals = require "globals"
-
 local R = require "R"
 local sound_player = require "sound_player"
 local util = require "util"
+local clock = require "clock"
 
 function love.load()
     -- Load all game assets
@@ -15,16 +14,8 @@ end
 
 ---@param dt number
 function love.update(dt)
-    -- Move clock forward, rolling over if necessary
-    globals.time = globals.time + dt
-    if globals.time >= globals.TIME_ROLLOVER then
-        globals.time = globals.time - globals.TIME_ROLLOVER
-    end
-
-    -- Update all running timers
+    clock.advance(dt)
     util.timers:update(dt)
-
-    -- Update the sound player
     sound_player.update()
 end
 
