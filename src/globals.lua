ea786a9d171fe4e261053f2a970235c1373f70f6
Charlie Murphy <cmsmurp00@gmail.com>
2025-02-25 20:14:37 -0500

    clock.advance(dt) function

--- src/globals.lua
+++ src/globals.lua
@@ -1,7 +1,17 @@
 -- (c) 2025 Charlie Murphy
 -- This code is licensed under MIT license (see LICENSE.txt for details)
 
-return {
+local clock = {
     time=0.0,
-    TIME_ROLLOVER=3600.0
+    TIME_ROLLOVER=3600.0,
 }
+
+---@param dt number Number of seconds elapsed this frame
+function clock.advance(dt)
+    clock.time = clock.time + dt
+    while clock.time >= clock.TIME_ROLLOVER do
+        clock.time = clock.time - clock.TIME_ROLLOVER
+    end
+end
+
+return clock
