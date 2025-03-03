1f851816fe034f6e0766e763ea1a2d58c2c11658
Charlie Murphy <cmsmurp00@gmail.com>
2025-03-03 04:34:36 -0500

    Reorganize R.lua

--- src/R.lua
+++ src/R.lua
@@ -8,39 +8,22 @@ local aseprite_sheet = require "support.aseprite_sheet"
 local R = {loaded=false}
 
 function R.loadResources()
+    ---------------------------------
+    -- EXPLICITLY PRELOADED ASSETS --
+    ---------------------------------
+
     ---@class R.images
     R.images = {
     }
-
-    -- Handler for lazily loaded images
-    setmetatable(R.images, {__index=function(t, k)
-        local img = love.graphics.newImage("assets/images/" .. k .. ".png")
-        rawset(t, k, img)
-        return img
-    end})
 
     ---@class R.sheets
     R.sheets = {
     }
 
-    -- Handler for lazily loaded sheets
-    setmetatable(R.sheets, {__index=function(t, k)
-        local sheet = aseprite_sheet(R.images[k], json.load("assets/images/" .. k .. ".json"))
-        rawset(t, k, sheet)
-        return sheet
-    end})
-
     ---@class R.fonts
     R.fonts = {
         -- example=love.graphics.newFont("assets/fonts/example.fnt"),
     }
-
-    -- Handler for lazily loaded fonts
-    setmetatable(R.fonts, {__index=function(t, k)
-        local fnt = love.graphics.newFont("assets/fonts/" .. k .. ".fnt")
-        rawset(t, k, fnt)
-        return fnt
-    end})
 
     ---@class R.sounds
     R.sounds = {
@@ -50,17 +33,42 @@ function R.loadResources()
         -- example=love.sound.newSoundData("assets/sounds/explosion.wav"),
     }
 
+    ---@class R.music
+    R.music = {
+        -- Probably just put musics' filenames in this table since music is streamed
+    }
+
+    ---------------------------
+    -- LAZY LOADING HANDLERS --
+    ---------------------------
+
+    -- Handler for lazily loaded images
+    setmetatable(R.images, {__index=function(t, k)
+        local img = love.graphics.newImage("assets/images/" .. k .. ".png")
+        rawset(t, k, img)
+        return img
+    end})
+
+    -- Handler for lazily loaded sheets
+    setmetatable(R.sheets, {__index=function(t, k)
+        local sheet = aseprite_sheet(R.images[k], json.load("assets/images/" .. k .. ".json"))
+        rawset(t, k, sheet)
+        return sheet
+    end})
+
+    -- Handler for lazily loaded fonts
+    setmetatable(R.fonts, {__index=function(t, k)
+        local fnt = love.graphics.newFont("assets/fonts/" .. k .. ".fnt")
+        rawset(t, k, fnt)
+        return fnt
+    end})
+
     -- Handler for lazily loaded sounds
     setmetatable(R.sounds, {__index=function(t, k)
         local wav = love.sound.newSoundData("assets/sounds/" .. k .. ".wav")
         rawset(t, k, wav)
         return wav
     end})
-
-    ---@class R.music
-    R.music = {
-        -- Probably just put musics' filenames in this table since music is streamed
-    }
 
     -- Handler for lazily loaded music
     setmetatable(R.music, {__index=function(t, k)
