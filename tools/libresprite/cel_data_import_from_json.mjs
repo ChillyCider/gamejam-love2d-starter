b312476ed1b5bd0aef7d30b81a309ff1a57c45f4
Charlie Murphy <cmsmurp00@gmail.com>
2025-03-12 01:52:16 -0400

    Remove aseprite tools

--- tools/libresprite/cel_data_import_from_json.mjs
+++
@@ -1,8 +0,0 @@
-// This script EXTRACTS the cel data from a Libresprite JSON sheet
-// and puts it INTO the currently open Libresprite file.
-//
-// Importantly, it only does so for the layer called "data". That means
-// you can write tools/etc that modify the cel data on the JSON sheet and
-// then easily get the data back into the Libresprite sheet. All this to make
-// a workflow for getting hitboxes and hurtboxes on your aseprite
-// animations.
